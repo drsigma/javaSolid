a13ee9f6ca53e01213da1cf6015a096fd61166d2
drsigma <rafalince@live.com>
2023-08-20 17:21:18 -0300

    fix: DIP - Dependency Inversion Principle - Aplicação

--- src/service/ReajusteService.java
+++ src/service/ReajusteService.java
@@ -13,16 +13,7 @@ public class ReajusteService {
     }
 
     public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
-        //jeito certo ao aplicar OCP já havia sido aplicado o DIP
         this.validacoes.forEach(v -> v.validar(funcionario, aumento));
-
-        //jeito errado ao aplicar OCP já havia sido aplicado DIP
-        ValidacaoPercentualReajuste validacaoPercentualReajuste = new ValidacaoPercentualReajuste();
-        ValidacaoPeriodicidadeEntreReajuste validacaoPeriodicidadeEntreReajuste = new ValidacaoPeriodicidadeEntreReajuste();
-
-        validacaoPercentualReajuste.validar(funcionario, aumento);
-        validacaoPeriodicidadeEntreReajuste.validar(funcionario, aumento);
-        //Ao se aplicar o OCP automaticamente já se aplicou o DIP
 
         BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
         funcionario.atualizaSalario(salarioReajustado);
