ee9efee6797c80fe9e2421011d50a43f8b89e9ab
drsigma <rafalince@live.com>
2023-08-20 16:05:57 -0300

    chore: OCP - Open Closed Principle - Uma classe que não para de crescer

--- src/service/ReajusteService.java
+++ src/service/ReajusteService.java
@@ -5,6 +5,8 @@ import rh.exception.ValidacaoException;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class ReajusteService {
     public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
@@ -13,6 +15,15 @@ public class ReajusteService {
         if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
             throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
         }
+
+        LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+        LocalDate dataAtual = LocalDate.now();
+        long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+
+        if (mesesDesdeUltimoReajuste < 6) {
+            throw new ValidacaoException("Intervalo entre reajustes deve ser no minímo 6 meses");
+        }
+
         BigDecimal salarioReajustado = salarioAtual.add(aumento);
         funcionario.atualizaSalario(salarioReajustado);
 
