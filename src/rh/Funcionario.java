20e9a4c4f0fa7501c5476fd2ae032b20413d0ba6
drsigma <rafalince@live.com>
2023-08-20 15:55:06 -0300

    chore: srp single responsability principle - melhorando a coesão

--- src/rh/Funcionario.java
+++ src/rh/Funcionario.java
@@ -21,14 +21,10 @@ public class Funcionario {
         this.salario = salario;
     }
 
-    public void reajustarSalario(BigDecimal aumento) {
-        BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-        }
-        this.salario = this.salario.add(aumento);
-        this.dataUltimoReajuste = LocalDate.now();
+    public void atualizaSalario(BigDecimal novoSalario){
+        this.salario = novoSalario;
     }
+
 
     public String getNome() {
         return nome;
@@ -56,10 +52,6 @@ public class Funcionario {
 
     public BigDecimal getSalario() {
         return salario;
-    }
-
-    public void setSalario(BigDecimal salario) {
-        this.salario = salario;
     }
 
     public LocalDate getDataUltimoReajuste() {
---
+++ src/service/ReajusteService.java
@@ -0,0 +1,20 @@
+package service;
+
+import rh.Funcionario;
+import rh.exception.ValidacaoException;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ReajusteService {
+    public void reajustarSalario(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal salarioAtual = funcionario.getSalario();
+        BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+            throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+        }
+        BigDecimal salarioReajustado = salarioAtual.add(aumento);
+        funcionario.atualizaSalario(salarioReajustado);
+
+    }
+}
